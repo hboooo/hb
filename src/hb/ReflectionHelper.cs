ab93ff80f20cb97b0b5771f12f609a8df50b668a
hbo <zhanghb_0604@163.com>
2021-08-08 00:46:30 +0800

    refection

---
+++ src/hb/ReflectionHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hb
+{
+    /// <summary>
+    /// author     :habo
+    /// date       :2021/8/7 23:57:41
+    /// description:
+    /// </summary>
+    public class ReflectionHelper
+    {
+        /// <summary>
+        /// 获取实现了指定接口类型的基类实例
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="assembly"></param>
+        /// <returns></returns>
+        public static T[] GetImplementObjects<T>(Assembly assembly)
+        {
+            if (assembly == null) throw new ArgumentNullException("assembly");
+
+            return assembly.GetExportedTypes().Where(c =>
+            {
+                if (c.IsClass && !c.IsAbstract)
+                {
+                    var interfaces = c.GetInterfaces();
+                    if (interfaces != null) return interfaces.Contains(typeof(T));
+                }
+                return false;
+            }).Select(c => (T)c.GetConstructor(new Type[0]).Invoke(new object[0])).ToArray();
+        }
+    }
+}
